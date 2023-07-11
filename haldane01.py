ed2ba1f694c9254a05001c49ac3051704a88aa1f
sogenikegami <ikegami-sogen443@g.ecc.u-tokyo.ac.jp>
2023-07-11 21:37:06 +0900

    add fourier y

--- haldane01.py
+++ haldane01.py
@@ -9,8 +9,8 @@ from local_chern_marker import local_chern_marker01
 
 
 a = 1#lattice constant
-n = 5 #systemsize x axis
-m = 5 #systemsize y axis
+n = 20 #systemsize x axis
+m = 20 #systemsize y axis
 
 #boundary condition 
 xPBC = False
@@ -70,6 +70,43 @@ def hamiltonian(pointdata,M,t1,t2,phi):
     H = hsite(pointdata,M) - hhop1(pointdata,t1) - hhop2(pointdata,t2,phi)
     return H
 
+def fourier_y(vertexdata,H,ky):
+    N = len(vertexdata)
+
+    h = np.zeros((2*(n+1),2*(n+1)))*0j
+    for i in range(2*(n+1)):
+        for j in range(2*(n+1)):
+            for k in range(m+1):
+                for l in range(m+1):
+                    yk = vertexdata[i+2*(n+1)*k]["pos"][1]
+                    yl = vertexdata[j+2*(n+1)*l]["pos"][1]
+                    phase = math.cos(ky*yl-ky*yk) + 1j*math.sin(ky*yl-ky*yk)
+                    h[i][j] += phase/2/math.pi * H[i+2*(n+1)*k][j+l*2*(n+1)]
+    eigval,eigvec = np.linalg.eig(h)
+    energy = []
+    for i in range(len(eigval)):
+        energy.append(eigval[i].real)
+    """
+    fig=plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    ax.scatter(ky,energy)
+    plt.savefig(filepath + imgname)
+    fig.show()
+    """
+    return energy
+
+def y_band_plot(vertexdata,H,imgname):
+    L = 2*math.pi/math.sqrt(3)/a/(m+1)
+    fig=plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    for i in range(m+1):
+        energy = fourier_y(vertexdata,H,i*L,)
+        for j in range(len(energy)):
+            plt.plot(i*L,energy[j],'.',color='blue')
+    plt.savefig(filepath + imgname)
+    fig.show()
+    return 0
+
 
 def eigenenergy(H,filepath,imgname):
     eigval,eigvec = np.linalg.eigh(H)
@@ -79,12 +116,14 @@ def eigenenergy(H,filepath,imgname):
         eigenergy.append(eigval[i].real)
     eigenergy.sort()
     fermi_energy = (eigenergy[int(len(H[0])/2)]+eigenergy[int(len(H[0])/2)-1])/2
+    
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     ax.scatter(num,eigenergy,s=10)
     plt.hlines(fermi_energy,0,len(eigval),linestyle="dashed",color='red')
     fig.show()
     plt.savefig(filepath + imgname)
+    
     return fermi_energy
 
 def main():
@@ -94,12 +133,18 @@ def main():
 
 
     imgname_crosshair = "crosshair" + str(n) + "times" + str(m)
-    crosshair_marker01.plot(H,pointdata[0],fermi_energy,Rx,Ry,filepath,imgname_crosshair)
+    #crosshair_marker01.plot(H,pointdata[0],fermi_energy,Rx,Ry,filepath,imgname_crosshair)
     imgname_local = "local_C_from_ch" + str(n) + "times" + str(m) 
-    crosshair_marker01.local_marker(H,pointdata[0],fermi_energy,filepath,imgname_local)
+    #crosshair_marker01.local_marker(H,pointdata[0],fermi_energy,filepath,imgname_local)
 
     imgname_localC = "local_C" + str(n) + "times" + str(m) 
-    local_chern_marker01.plot(H,pointdata[0],fermi_energy,filepath,imgname_localC)
+    #local_chern_marker01.plot(H,pointdata[0],fermi_energy,filepath,imgname_localC)
+
+    imgname_yfourier = "ky_band" + str(n) + "times" + str(m) 
+    xnumber =1
+    y_band_plot(pointdata[0],H,imgname_yfourier)
+    imgname_yfourier = "ky_band" + str(n) + "times" + str(m) +"(02)"
+    return 0
 
 if __name__ == "__main__":
     main()
